c495a7d3b5e7855c2898955b028aedaaa32ba0db
PavelDeuce <deucela.pavel@gmail.com>
2022-07-25 22:22:18 +0300

    completed first step: static routes

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,3 +1,21 @@
-test('init', () => {
-  expect(1).toBe(1);
+import makeRouter from '../src/router.js';
+
+test('router', () => {
+  const routes = [
+    {
+      path: '/courses',
+      handler: () => 'courses!',
+    },
+    {
+      path: '/courses/basics',
+      handler: () => 'basics',
+    },
+  ];
+
+  const router = makeRouter(routes);
+  const path = '/courses';
+  const handler = router.serve(path);
+
+  expect(handler()).toBe('courses!');
+  expect(() => router.serve('/no_such_way')).toThrow('Unknown path');
 });
--- src/router.js
+++ src/router.js
@@ -1,3 +1,11 @@
-export default () => {
-  console.log('init');
+export default (routes) => {
+  const serve = (path) => {
+    const foundPath = routes.find((route) => path === route.path);
+
+    if (!foundPath) throw new Error('Unknown path');
+
+    return foundPath.handler;
+  };
+
+  return { serve };
 };
